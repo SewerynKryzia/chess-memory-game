98830b37bb1768fdf70a9c4b89aea7644fe34b76
SewerynKryzia <seweryn.kryzia@gmail.com>
2024-11-20 14:48:24 +0100

    add random answers to questions

--- lib/functions.ts
+++ lib/functions.ts
@@ -1,5 +1,5 @@
 import { PIECES } from "@/lib/constants";
-import { QUESTIONS_NUMBER } from "./settings";
+import { ANSWERS_NUMBER, QUESTIONS_NUMBER } from "@/lib/settings";
 
 export function changeFenToArray(fenPosition: string) {
   const arrayPosition = [];
@@ -23,28 +23,66 @@ export function changeFenToArray(fenPosition: string) {
 }
 
 export function drawQuestion(piecesInfo: { position: number; type: string }[]) {
+  // * add empty squares
   // question focus on squares where are chess pieces, but additionally there are added random empty squares and their number base on number of chess pieces
-  // there are max 32 chess pieces on board, so there are always empty squares remaining
-  const emptySquaresToAdd = Math.floor(piecesInfo.length / 3) + 1;
+  const emptySquaresToAdd = Math.floor(piecesInfo.length / 3) + 1; // there are max 32 chess pieces on board, so there are always empty squares available
+  const piecesAndEmpty = [...piecesInfo];
   for (let i = 0; i <= emptySquaresToAdd; i++) {
     function randomPosition() {
       const randomNumber = Math.floor(Math.random() * 64);
-      const index = piecesInfo.findIndex(
+      const index = piecesAndEmpty.findIndex(
         (piece) => piece.position === randomNumber,
       );
       if (index === -1) {
-        piecesInfo.push({ position: randomNumber, type: "" });
+        piecesAndEmpty.push({ position: randomNumber, type: "" });
       } else {
         randomPosition();
       }
     }
     randomPosition();
   }
-  // draw 3 random positions
-  const shuffledArray = piecesInfo.sort(() => 0.5 - Math.random());
+  // * draw 3 random positions
+  const shuffledArray = piecesAndEmpty.sort(() => 0.5 - Math.random());
   const questions = shuffledArray.slice(0, QUESTIONS_NUMBER);
 
-  // draw 3 incorrect answers for each question
-
-  return questions;
+  // * draw 3 incorrect answers for each question
+  const allAnswers = [
+    "K",
+    "Q",
+    "R",
+    "B",
+    "N",
+    "P",
+    "k",
+    "q",
+    "r",
+    "b",
+    "n",
+    "p",
+    "",
+    "",
+    "",
+  ];
+  // add wieght to answers - more likely answers include pieces that were on board
+  piecesInfo.forEach((piece) => allAnswers.push(piece.type, piece.type));
+  // add answers to the questions
+  const questionsWithAnswers = questions.map((question) => {
+    const updatedQuestion = { ...question, answers: [question.type] };
+    for (let i = 1; i < ANSWERS_NUMBER; i++) {
+      function randomAnswer() {
+        const randomIndex = Math.floor(Math.random() * allAnswers.length);
+        const exist = updatedQuestion.answers.includes(allAnswers[randomIndex]);
+        if (!exist) {
+          updatedQuestion.answers.push(allAnswers[randomIndex]);
+        } else {
+          randomAnswer();
+        }
+      }
+      randomAnswer();
+    }
+    // randomize answers
+    updatedQuestion.answers.sort(() => 0.5 - Math.random());
+    return updatedQuestion;
+  });
+  return questionsWithAnswers;
 }
--- lib/settings.ts
+++ lib/settings.ts
@@ -1,2 +1,3 @@
 // lifes, time etc.
 export const QUESTIONS_NUMBER = 3;
+export const ANSWERS_NUMBER = 4;
